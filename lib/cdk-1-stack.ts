87615447e14d5590334a61b9704b9bdd3c6f71d4
Raymond <58218638+raybags-web-dev@users.noreply.github.com>
2022-10-16 02:09:02 +0200

    initial commit - test app with cdk ec2 and ecs-patterns

--- lib/cdk-1-stack.ts
+++ lib/cdk-1-stack.ts
@@ -1,15 +1,19 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 
 export class Cdk1Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", { maxAzs: 2 });
+    const cluster = new ecs.Cluster(this, "MyCluster", { vpc: vpc });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Cdk1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargetService", {
+      cluster: cluster,
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      publicLoadBalancer: true
+    });
   }
 }
